c334db6c58c69bca98e0a94736a7cf33fd54d822
cassio.szcampos@gmail.com <cassio.szcampos@gmail.com>
2022-03-13 13:19:29 -0300

    remocao de comentarios desnecessarios

--- clinica/cadastro-funcionario.php
+++ clinica/cadastro-funcionario.php
@@ -15,8 +15,6 @@ if(!check_sessao()){
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
-  <!-- <link href="css/cadastro-funcionario.css" rel="stylesheet">
-  <link href="css/navbar.css" rel="stylesheet"> -->
   <link href="css/style.css" rel="stylesheet">
 </head>
 
